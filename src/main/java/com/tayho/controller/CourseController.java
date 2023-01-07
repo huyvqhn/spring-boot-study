8c42007cc3b0c1fac837d1942206317536d25018
Admin <huyvqhn@gmail.com>
2023-01-07 17:42:45 +0700

    First commit

---
+++ src/main/java/com/tayho/controller/CourseController.java
@@ -0,0 +1,22 @@
+package com.tayho.controller;
+
+import com.tayho.model.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping(value = "/courses")
+    public List<Course> retrieveCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn Azure", "in28minutes"),
+                new Course(4, "Learn GCP", "in28minutes")
+        );
+    }
+}
